02b46b0dd8f3ce811fa1156141bd83dd3561cd67
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 20:45:38 -0300

    TextView

---
+++ app/src/main/java/com/github/gregoryced/orgs/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.gregoryced.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
